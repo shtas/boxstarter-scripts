79a54231d251ad9792ee9e24c08675ceb3aac89c
shtas <stian@thaulow.nu>
2014-07-02 06:31:04 +0200

    Adds initial script

---
+++ virtualbox-dev.ps1
@@ -0,0 +1,15 @@
+#-------------------------------------------------
+Update-ExecutionPolicy Unrestricted
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+Disable-InternetExplorerESC
+Disable-UAC
+
+cinst VisualStudioExpress2013WindowsDesktop
+cinst VisualStudio2013ExpressWeb
+
+cinst linqpad4
+
+cinst git
+cinst PoshGit
+
+cinst 7zip
